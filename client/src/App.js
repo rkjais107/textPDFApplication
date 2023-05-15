6c42ec73f6ecb89d5c7223bb723a8cf4aabaf338
manas5996 <manas5996@gmail.com>
2023-05-15 20:40:58 +0530

    added backend and frontend code

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [data, setData] = useState([{}]);
+  useEffect(() => {
+    fetch("/members")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+  return (
+    <div>
+      {typeof data.members === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        data.members.map((member, i) => <p key={i}>{member}</p>)
+      )}
+    </div>
+  );
+};
+
+export default App;
